b40cd17dda806c0d209cfc766e89c30240ec06c2
Balaji Reddy Dwarampudi <109616551+baludbr@users.noreply.github.com>
2023-07-11 16:56:19 +0530

    uploaded

---
+++ PROJECT_MANAGEMENT_SYSTEM/src/main/java/com/klu/controller/UserBean.java
@@ -0,0 +1,93 @@
+package com.klu.controller;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.validation.constraints.Size;
+
+import com.klu.entity.User;
+import com.klu.model.UserActions;
+
+@ManagedBean(name="user",eager=true)
+public class UserBean {
+int id;
+@Size(min=4)
+String name;
+String email;
+@Size(min=8)
+String password;
+String role;
+String response;
+@EJB(lookup="java:global/PROJECT_MANAGEMENT_SYSTEM/UserImpl!com.klu.model.UserActions")
+UserActions us;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+public String getResponse() {
+	return response;
+}
+public void setResponse(String response) {
+	this.response = response;
+}
+public String register()
+{
+	User u=new User();
+	u.setName(name);
+	u.setEmail(email);
+	u.setPassword(password);
+	u.setRole(role);
+	String s=us.register(u);
+	if(s.equals("Registered Successfully"))
+	{
+		return "login.jsf??faces-redirect=true";
+	}
+	else
+	{
+		response=s;
+		return "register.jsf";
+	}
+}
+public String login()
+{
+	User u=new User();
+	u.setEmail(email);
+	u.setPassword(password);
+	String s=us.login(u);
+	if(s.equals("Login Successful"))
+	{
+		return "home.jsf";
+	}
+	else
+	{
+		response=s;
+		return "login.jsf";
+	}
+	
+}
+}
